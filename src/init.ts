df4d09b51ff0ac808cff51cbce2556e3573f7cdf
leegeunhyeok <dev.ghlee@gmail.com>
2020-05-27 11:22:45 +0900

    UPDATE: init function implements
    Load docker enging API version, render title

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,30 @@
-// docker version -f '{{json .Client.DefaultAPIVersion}}'
-export default () => {
-  return {
+import { command } from './core';
+import { async } from './utils';
+import figlet from 'figlet';
+import standard from 'figlet/importable-fonts/Standard.js';
+import { version } from '../package.json';
 
+export default async () => {
+  const renderTitle = async (apiVersion: string): Promise<void> => {
+    figlet.parseFont('Standard', standard);
+    const [err, data] = await async(figlet, 'Dockctl');
+
+    if (!err) {
+      console.log(data);
+      console.log(`Dockctl v${version}`.padStart(34));
+      console.log(`Docker API Engine v${apiVersion}`.padStart(34));
+    }
   };
+
+  const getDockerAPIVersion = async (): Promise<string> => {
+    const { data, code } = await command(
+      'docker',
+      ['version', '-f', '{{json .Client.DefaultAPIVersion}}']
+    );
+
+    return code === 0 ? data.toString() : 'unknown';
+  };
+
+  const apiVersion = await getDockerAPIVersion();
+  await renderTitle(apiVersion);
 };
