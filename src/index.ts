7123fa2c022630e84185f83dede3e1c20afec1cd
leegeunhyeok <dev.ghlee@gmail.com>
2020-05-28 17:01:12 +0900

    UPDATE: Coroutine handling logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
 import init from './init';
 import { co } from './core';
+import { DockctlContext } from './interfaces';
 import { DockctlSignal, DockctlMenu } from './enums';
 
 (async () => {
   const apiVersion = await init();
-  const loop = co();
-  const ctx = {
+  const ctx: DockctlContext = {
     __VERSION__: apiVersion,
     __SIGNAL__: DockctlSignal.OK,
     __NEXT__: DockctlMenu.MAIN
   };
+  const loop = co(ctx);
   let res = null;
 
-  while (!(res = loop.next(ctx)).done) {
-    console.log(res);
-    ctx.__SIGNAL__ = DockctlSignal.EXIT;
+  while (!(res = loop.next()).done) {
+    const { k, prompt } = res.value;
+    ctx.__NEXT__ = (await prompt)[k];
+
+    if (ctx.__NEXT__ === DockctlMenu.EXIT) {
+      ctx.__SIGNAL__ = DockctlSignal.EXIT;
+    }
   }
 })();
